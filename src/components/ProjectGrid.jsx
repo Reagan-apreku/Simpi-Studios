5832079d904340b9712ab65ae0ec93f789fd12c8
MACBOOK PRO M1 <macbookprom1@Reagans-MacBook-Pro.local>
2026-05-19 16:31:20 +0000

    Rename pre wedding photoshoot title to NANA YAW & JOETTE - PRE WEDDING PHOTOSHOOT

--- src/components/ProjectGrid.jsx
+++ src/components/ProjectGrid.jsx
@@ -142,7 +142,7 @@ const projects = [
   },
   {
     id: 6,
-    title: 'PRE WEDDING PHOTOSHOOT',
+    title: 'NANA YAW & JOETTE - PRE WEDDING PHOTOSHOOT',
     cover: '/images/gallery/10_000/007A8334SIMPI25.jpg',
     images: [
       '/images/gallery/10_000/007A8196SIMPI25.jpg',
